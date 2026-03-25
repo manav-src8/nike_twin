ef23c9db1fd77c792ee280b6ee6809e9a1a82c7b
Manav Koirala <manav.focus8@example.com>
2026-03-25 16:42:57 +0545

    Header Completed

--- components/searchBar.jsx
+++ components/searchBar.jsx
@@ -1,20 +1,28 @@
 "use client";
+import { Search } from "lucide-react";
 
 const SearchBar = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault;
+  };
   return (
-    <form
-      onSubmit={""}
-      className="flex items-center border-[1.5px] rounded-full px-2   border-black "
-    >
-      <input
-        type="text"
-        placeholder="Search"
-        // value={"query"}
-        // onChange={""}
-        className="outline-none text-sm "
-      />
-      <button></button>
-    </form>
+    <div className=" h-full">
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center gap-2 border-[1.5px] h-full rounded-full px-2 border-transparent bg-slate-500"
+      >
+        <button>
+          <Search />
+        </button>
+        <input
+          type="text"
+          placeholder="Search"
+          // value={"query"}
+          // onChange={""}
+          className="outline-none text-[16px] w-40 h-full bg-transparent  "
+        />
+      </form>
+    </div>
   );
 };
 
